05bf5100a546872d778f11e74ec6e3cf64f4b73a
JonnyDdesign <jondesserres@gmail.com>
2024-06-25 13:35:58 -0700

    Added a javascript file along with a modal to allow users to click an image in the gallery and view it full screen.

---
+++ index.js
@@ -0,0 +1,26 @@
+// Get modal element
+var modal = document.getElementById("imageModal");
+
+// Get modal image and caption
+var modalImg = document.getElementById("modalImage");
+var captionText = document.getElementById("caption");
+
+// Get all images in the gallery
+var images = document.querySelectorAll('.photoGallery img');
+
+// Add click event listener to each image
+images.forEach(function(img) {
+    img.addEventListener('click', function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    });
+});
+
+// Get the close button
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Add click event listener to the close button
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
